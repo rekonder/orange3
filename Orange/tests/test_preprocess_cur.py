4554a8f000e69e93657a6d844b5a8ff4d649b8e8
rekonder <andrej.jugovic7@gmail.com>
2016-04-12 16:35:02 +0200

    Changing test_preprocess_cur.py

--- Orange/tests/test_preprocess_cur.py
+++ Orange/tests/test_preprocess_cur.py
@@ -10,23 +10,18 @@ class TestCURProjector(unittest.TestCase):
         cls.ionosphere = Table("ionosphere")
 
     def test_project_cur_default(self):
+        self.helper_project_cur(ProjectCUR())
+
+    def test_project_cur(self):
+        self.helper_project_cur(ProjectCUR(rank=3, max_error=1))
+
+    def helper_project_cur(self, projector):
         data = self.ionosphere
         projector = ProjectCUR()
         data_cur = projector(data)
         for i in range(data_cur.X.shape[1]):
             sbtr = (data.X - data_cur.X[:, i][:, None]) == 0
             self.assertTrue(((sbtr.sum(0) == data.X.shape[0])).any())
-        self.assertTrue(data_cur.X.shape[1] <= data.X.shape[1])
-        self.assertTrue((data.metas == data_cur.metas).all())
-        self.assertTrue((data.Y == data_cur.Y).any())
-
-    def test_project_cur(self):
-        data = self.ionosphere
-        projector = ProjectCUR(rank=3, max_error=1)
-        data_cur = projector(data)
-        for i in range(data_cur.X.shape[1]):
-            sbtr = (data.X - data_cur.X[:, i][:, None]) == 0
-            self.assertTrue(((sbtr.sum(0) == data.X.shape[0])).any())
-        self.assertTrue(data_cur.X.shape[1] <= data.X.shape[1])
+        self.assertLessEqual(data_cur.X.shape[1], data.X.shape[1])
         self.assertTrue((data.metas == data_cur.metas).all())
         self.assertTrue((data.Y == data_cur.Y).any())
