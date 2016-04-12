523aadf4429452ba90d6b710463a680edf94b0b0
rekonder <andrej.jugovic7@gmail.com>
2016-04-12 16:41:56 +0200

    Changing in test_reprocess_pca.py

--- Orange/tests/test_preprocess_pca.py
+++ Orange/tests/test_preprocess_pca.py
@@ -9,18 +9,15 @@ class TestPCAProjector(unittest.TestCase):
     def setUpClass(cls):
         cls.ionosphere = Table("ionosphere")
 
-    def test_project_pca_default(self):
+    def project_pca_helper(self, projector, res):
         data = self.ionosphere
-        projector = ProjectPCA()
         data_pc = projector(data)
-        self.assertEqual(data_pc.X.shape[1], data.X.shape[1])
+        self.assertEqual(data_pc.X.shape[1], res)
         self.assertTrue((data.metas == data_pc.metas).all())
         self.assertTrue((data.Y == data_pc.Y).any())
 
+    def test_project_pca_default(self):
+        self.project_pca_helper(ProjectPCA(), self.ionosphere.X.shape[1])
+
     def test_project_pca(self):
-        data = self.ionosphere
-        projector = ProjectPCA(n_components=5)
-        data_pc = projector(data)
-        self.assertEqual(data_pc.X.shape[1], 5)
-        self.assertTrue((data.metas == data_pc.metas).all())
-        self.assertTrue((data.Y == data_pc.Y).any())
+        self.project_pca_helper(ProjectPCA(n_components=5), 5)
