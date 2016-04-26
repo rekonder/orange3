e114472c8f35610258308dfc7dfce4d61fbc9e33
rekonder <andrej.jugovic7@gmail.com>
2016-04-26 16:40:42 +0200

    Changed test_xlsx_reader.py

--- Orange/tests/test_xlsx_reader.py
+++ Orange/tests/test_xlsx_reader.py
@@ -11,6 +11,18 @@ def read_file(name):
         os.path.join(os.path.dirname(__file__), "xlsx_files", name))
 
 
+def meta_test_helper(domain, table):
+    test = unittest.TestCase('__init__')
+    test.assertEqual(len(domain.metas), 3)
+    for n, var in zip("acf", domain.metas):
+        test.assertEqual(var.name, n)
+    test.assertIsInstance(domain.metas[0], DiscreteVariable)
+    test.assertEqual(domain.metas[0].values, ["green", "red"])
+    test.assertIsInstance(domain.metas[1], ContinuousVariable)
+    np.testing.assert_almost_equal(table.metas[:, 0], np.array([1, 1, 0] * 7 + [1, 1]))
+    np.testing.assert_almost_equal(table.metas[:, 1], np.array([0, 1, 2, 3] * 5 + [0, 1, 2]))
+    np.testing.assert_equal(table.metas[:, 2], np.array(list("abcdefghijklmnopqrstuvw")))
+
 class TestExcelHeader0(unittest.TestCase):
     def test_read(self):
         table = read_file("header_0.xlsx")
@@ -56,65 +68,36 @@ class TestExcelHeader1(unittest.TestCase):
         table = read_file("header_1_flags.xlsx")
         domain = table.domain
 
-        self.assertEqual(len(domain.attributes), 1)
-        attr = domain.attributes[0]
-        self.assertEqual(attr.name, "d")
-        self.assertIsInstance(attr, ContinuousVariable)
         np.testing.assert_almost_equal(table.X, np.arange(23).reshape(23, 1))
+        np.testing.assert_almost_equal(table.Y, np.array([.5, .1, 0, 0] * 5 + [.5, .1, 0]))
 
+        self.assertEqual(len(domain.attributes), 1)
         self.assertEqual(len(domain.class_vars), 1)
-        class_ = domain.class_var
-        self.assertEqual(class_.name, "b")
-        self.assertIsInstance(class_, ContinuousVariable)
-        np.testing.assert_almost_equal(
-            table.Y, np.array([.5, .1, 0, 0] * 5 + [.5, .1, 0]))
+        for value, realName in ((domain.attributes[0], "d"),
+                                (domain.class_var, "b")):
+            self.assertEqual(value.name, realName)
+            self.assertIsInstance(value, ContinuousVariable)
+        meta_test_helper(domain, table)
 
-        self.assertEqual(len(domain.metas), 3)
-        for n, var in zip("acf", domain.metas):
-            self.assertEqual(var.name, n)
-        self.assertIsInstance(domain.metas[0], DiscreteVariable)
-        self.assertEqual(domain.metas[0].values, ["green", "red"])
-        self.assertIsInstance(domain.metas[1], ContinuousVariable)
-        np.testing.assert_almost_equal(
-            table.metas[:, 0], np.array([1, 1, 0] * 7 + [1, 1]))
-        np.testing.assert_almost_equal(
-            table.metas[:, 1], np.array([0, 1, 2, 3] * 5 + [0, 1, 2]))
 
 class TestExcelHeader3(unittest.TestCase):
     def test_read(self):
         table = read_file("header_3.xlsx")
         domain = table.domain
 
-        self.assertEqual(len(domain.attributes), 2)
-        attr = domain.attributes[0]
-        self.assertEqual(attr.name, "d")
-        self.assertIsInstance(attr, ContinuousVariable)
         np.testing.assert_almost_equal(table.X[:, 0], np.arange(23))
-        attr = domain.attributes[1]
-        self.assertEqual(attr.name, "g")
-        self.assertIsInstance(attr, DiscreteVariable)
-        np.testing.assert_almost_equal(table.X[:, 1],
-                                       np.array([1, 0] + [float("nan")] * 21))
+        np.testing.assert_almost_equal(table.X[:, 1], np.array([1, 0] + [float("nan")] * 21))
+        np.testing.assert_almost_equal(table.Y, np.array([.5, .1, 0, 0] * 5 + [.5, .1, 0]))
 
+        self.assertEqual(len(domain.attributes), 2)
         self.assertEqual(len(domain.class_vars), 1)
-        class_ = domain.class_var
-        self.assertEqual(class_.name, "b")
-        self.assertIsInstance(class_, ContinuousVariable)
-        np.testing.assert_almost_equal(
-            table.Y, np.array([.5, .1, 0, 0] * 5 + [.5, .1, 0]))
 
-        self.assertEqual(len(domain.metas), 3)
-        for n, var in zip("acf", domain.metas):
-            self.assertEqual(var.name, n)
-        self.assertIsInstance(domain.metas[0], DiscreteVariable)
-        self.assertEqual(domain.metas[0].values, ["green", "red"])
-        self.assertIsInstance(domain.metas[1], ContinuousVariable)
-        np.testing.assert_almost_equal(
-            table.metas[:, 0], np.array([1, 1, 0] * 7 + [1, 1]))
-        np.testing.assert_almost_equal(
-            table.metas[:, 1], np.array([0, 1, 2, 3] * 5 + [0, 1, 2]))
-        np.testing.assert_equal(
-            table.metas[:, 2], np.array(list("abcdefghijklmnopqrstuvw")))
+        for vals, realName, valueType in ((domain.attributes[0], "d", ContinuousVariable),
+                                          (domain.attributes[1], "g", DiscreteVariable),
+                                          (domain.class_var, "b", ContinuousVariable)):
+            self.assertEqual(vals.name, realName)
+            self.assertIsInstance(vals, valueType)
+        meta_test_helper(domain, table)
 
 if __name__ == "__main__":
     unittest.main()
